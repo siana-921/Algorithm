bf7c1cb086dd3268750cd54b9c246d9bbc28fa4f
sua <122634419+siana-921@users.noreply.github.com>
2023-09-30 21:50:53 +0900

    [unrated] Title: 달리기 경주, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+    let idx;
+    let name1;
+    let name2;
+    const idxList = {}
+
+    players.forEach((name,index)=>idxList[name]=index)
+    for(let call of callings){
+        idx = idxList[call]
+        name1 = players[idx]
+        name2 = players[idx-1]
+        idxList[call]-=1
+        idxList[name2]+=1
+        players[idx] = name2
+        players[idx-1] = name1
+    }    
+
+    return players;
+}
